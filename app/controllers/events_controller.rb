2e62585ff06f80e3d8b38eaeb202e8f04bfd9bb0
paul <ajuzepaul>
2020-07-03 23:03:39 +0300

    build connection between event and user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,13 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
 
   # GET /events
   # GET /events.json
   def index
     @events = Event.all
+    @Users = User.all
   end
 
   # GET /events/1
@@ -14,7 +17,7 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
-    @event = Event.new
+    @event = current_user.events.build
   end
 
   # GET /events/1/edit
@@ -24,7 +27,7 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    @event = Event.new(event_params)
+    @event = current_user.eventss.build(event_params)
 
     respond_to do |format|
       if @event.save
