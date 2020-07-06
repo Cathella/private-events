85fbd24189acc3f78cde1fad7e30a245385a3c30
Cathella <ncathie214@gmail.com>
2020-07-07 01:01:48 +0300

    Create sessions controler to sign user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resources :home, only: ["index"]
+  root 'home#index'
+
   resources :users
   resources :events
-  # devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :sessions, only: [:new, :create, :destroy]
 
-  resources :home, only: ["index"]
-  root to: "home#index"
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
