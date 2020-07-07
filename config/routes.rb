0d8b1a77afb4cc104c3312dfffc16b82c2cebbce
Cathella <ncathie214@gmail.com>
2020-07-07 10:50:45 +0300

    Edit sign up form view

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
 end
