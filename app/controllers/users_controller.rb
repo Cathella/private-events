d9b07dee84d648879d9441252eb5723fb1f896e1
paul <ajuzepaul>
2020-07-09 19:57:57 +0300

    add upcoming and past events to model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
   # GET /users
   # GET /users.json
   def index
@@ -12,6 +11,8 @@ class UsersController < ApplicationController
   def show
     set_user
     @attendance = @user.attendances
+    @previous_events = @user.previous_events
+    @upcoming_events = @user.upcoming_events
   end
 
   # GET /users/new
