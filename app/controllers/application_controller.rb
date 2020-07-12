451e2deb782e1ee3c3f9544f165d991a78d196f6
paul <ajuzepaul>
2020-07-12 18:04:56 +0300

    refactor helpers and layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  helper_method :attending?
 
   def current_user
     if session[:user_id]
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
     else
       @current_user = nil
     end
+  end
+
+  def attending?(event_id)
+    current_user.events_to_attend.where(id: event_id).any?
   end
 end
